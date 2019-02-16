aaa6ae5dd6ede47d782008f9a7e4d60e29413fa4
LJEsp <esparragoleandroj@gmail.com>
2019-02-17 00:35:33 +0800

    Test

--- server/server.js
+++ server/server.js
@@ -32,12 +32,13 @@ app.use("/api", posts);
 
 // >>> Serve static in production
 if (process.env.NODE_ENV === "production") {
+  //set static folder
   app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
 }
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 const port = process.env.PORT || 5000;
 
