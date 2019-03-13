b72c2625ce3f4aa516402702f43d263035930a40
LJEsp <esparragoleandroj@gmail.com>
2019-03-13 17:44:09 +0800

    This commented out code is causing the redirect bug

--- client/src/pages/App/components/Main/components/MainSideModal.js
+++ client/src/pages/App/components/Main/components/MainSideModal.js
@@ -1,4 +1,4 @@
-import React  from "react";
+import React from "react";
 import styled from "styled-components";
 import { Transition, animated } from "react-spring/renderprops";
 import { withRouter, Route, Switch } from "react-router-dom";
@@ -25,28 +25,24 @@ const MainSideModal = ({
   }
 
   return (
-    <Transition
-      native
-      items={location}
-      keys={key}
-      from={{ transform: "translateX(-100%)" }}
-      enter={{ transform: "translateX(0%)" }}
-      leave={{ transform: "translateX(-100%)" }}
-      immediate
-    >
-      {loc => style => (
-        <Switch location={loc}>
-          <Route
-            path={`${match.url}${routePath}`}
-            render={() => (
-              <StyledMainSideModal style={style}>
-                {children}
-              </StyledMainSideModal>
-            )}
-          />
-        </Switch>
-      )}
-    </Transition>
+    // <Transition
+    //   native
+    //   items={location}
+    //   keys={key}
+    //   from={{ transform: "translateX(-100%)" }}
+    //   enter={{ transform: "translateX(0%)" }}
+    //   leave={{ transform: "translateX(-100%)" }}
+    //   immediate
+    // >
+    //   {loc => style => (
+    //     <Switch location={loc}>
+    <Route
+      path={`${match.url}${routePath}`}
+      render={() => <StyledMainSideModal>{children}</StyledMainSideModal>}
+    />
+    //     </Switch>
+    //   )}
+    // </Transition>
   );
 };
 
