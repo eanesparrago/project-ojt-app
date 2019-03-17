79c9823e06b32f34dc75127497d68ba9af803dfa
LJEsp <esparragoleandroj@gmail.com>
2019-03-17 13:40:06 +0800

    Previous

--- client/src/pages/App/scenes/Announcements/Announcements.js
+++ client/src/pages/App/scenes/Announcements/Announcements.js
@@ -1,84 +1,24 @@
 import React, { Component } from "react";
-import styled from "styled-components";
-import { Transition } from "react-spring/renderprops";
-import { Route, Switch, withRouter, Link } from "react-router-dom";
-import { Button, Typography } from "../../../../components/elements";
-import { Item, Box, Container, Area } from "src/components/blocks";
+import { connect } from "react-redux";
 
-import CreateAnnouncement from "./scenes/CreateAnnouncement";
-import AnnouncementTable from "./components/AnnouncementTable";
-
-import adminScenesStyles from "src/pages/Admin/adminScenesStyles";
-
-const StyledAnnouncements = styled.div`
-  ${adminScenesStyles};
-`;
+import { Main } from "src/pages/App/components";
 
 export class Announcements extends Component {
-  state = {
-    isModalOpen: false
-  };
-
-  handleModalToggle = () => {
-    this.setState({ isModalOpen: !this.state.isModalOpen });
-  };
+  componentDidMount() {}
 
   render() {
-    const { match, location } = this.props;
-
     return (
-      <StyledAnnouncements>
-        <Area NAME="admin-content-header" padding="inset-base">
-          <Box wrap align="flex-start">
-            <Item margin="wrap-base">
-              <Typography variant="display-1">Announcements</Typography>
-            </Item>
+      <Main>
+        <Main.Header
+          title="Announcements"
+          buttonText="Create Announcement"
+          buttonPath="/create-announcement"
+        />
 
-            <Item margin="wrap-base">
-              <Button
-                variant="primary"
-                as={Link}
-                to={`${match.url}/create-announcement`}
-              >
-                <Item margin="inline-s">
-                  <i className="fas fa-plus" />
-                </Item>
-                Create Announcement
-              </Button>
-            </Item>
-          </Box>
-        </Area>
-
-        <Area NAME="admin-content-body" padding="inset-base">
-          <AnnouncementTable />
-        </Area>
-
-        {/* >>> Create Announcement Modal */}
-        <Transition
-          native
-          items={location}
-          keys={location.pathname}
-          from={{ transform: "translateX(-100%)" }}
-          enter={{ transform: "translateX(0%)" }}
-          leave={{ transform: "translateX(-100%)" }}
-        >
-          {loc => style => (
-            <Switch location={loc}>
-              <Route
-                path={`${match.url}/create-announcement`}
-                render={() => (
-                  <Container NAME="admin-create" animate={style}>
-                    <CreateAnnouncement />
-                  </Container>
-                )}
-              />
-            </Switch>
-          )}
-        </Transition>
-    
-      </StyledAnnouncements>
+        <Main className="Body" />
+      </Main>
     );
   }
 }
 
-export default withRouter(Announcements);
+export default connect()(Announcements);
